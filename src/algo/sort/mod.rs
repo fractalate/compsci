b80eecfdaf015d31b6e6493aab1c6aeea5e1855f
Kate Novak <kje@cochleoid.com>
2024-10-05 19:40:02 -0400

    Notes about things I plan to do.

--- src/algo/sort/mod.rs
+++ src/algo/sort/mod.rs
@@ -1,3 +1,10 @@
 pub mod bubble_sort;
 
 pub use bubble_sort::*;
+
+// TODO - Perform algorithmic analysis of bubble sort.
+// TODO - Implement selection sort. Perform algorithmic analysis.
+// TODO - Implement insertion sort. Perform algorithmic analysis.
+// TODO - Implement merge sort. Perform algorithmic analysis.
+// TODO - Implement quick sort. Perform algorithmic analysis.
+// TODO - What exactly is Tim sort? Implement Tim sort?
