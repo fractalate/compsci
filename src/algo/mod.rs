ee2fb44222802f7bc33fd0f1bf8d9fbfff768700
Kate Novak <kje@cochleoid.com>
2024-10-05 21:21:37 -0400

    Notes about things I plan to do.

--- src/algo/mod.rs
+++ src/algo/mod.rs
@@ -1,1 +1,3 @@
 pub mod sort;
+
+// TODO - Two Sum. Algos 3e 2.3-7 Theta(n lg n)
--- src/algo/sort/mod.rs
+++ src/algo/sort/mod.rs
@@ -6,5 +6,6 @@ pub use bubble_sort::*;
 // TODO - Implement selection sort. Perform algorithmic analysis.
 // TODO - Implement insertion sort. Perform algorithmic analysis.
 // TODO - Implement merge sort. Perform algorithmic analysis.
+// TODO - Implement merge sort with insertion sort. Perform algorithmic analysis.
 // TODO - Implement quick sort. Perform algorithmic analysis.
 // TODO - What exactly is Tim sort? Implement Tim sort?
