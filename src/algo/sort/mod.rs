a85f7ce01f6458056410b1a596f3c780c3fd5447
Kate Novak <kje@cochleoid.com>
2024-10-06 11:51:50 -0400

    An Implementation and Worst-case Analysis of Selection Sort

--- src/algo/sort/mod.rs
+++ src/algo/sort/mod.rs
@@ -1,10 +1,11 @@
 pub mod bubble_sort;
 pub mod insertion_sort;
+pub mod selection_sort;
 
 pub use bubble_sort::*;
 pub use insertion_sort::*;
+pub use selection_sort::*;
 
-// TODO - Implement selection sort. Perform algorithmic analysis.
 // TODO - Implement merge sort. Perform algorithmic analysis.
 // TODO - Implement merge sort with insertion sort. Perform algorithmic analysis.
 // TODO - Implement quick sort. Perform algorithmic analysis.
---
+++ src/algo/sort/selection_sort.rs
@@ -0,0 +1,34 @@
+pub fn selection_sort<T>(items: &mut [T])
+where
+    T: std::cmp::Ord {
+  // Analysis                            Worse Case
+  for i in 0..items.len()-1 {   // (n-1)
+    let mut bj = i;             // (n-1)
+    for j in i+1..items.len() { // (n-1) + (n-2) + ... + 1 = n*(n-1)/2
+      if items[j] < items[bj] {        // (n-1) + (n-2) + ... + 1 = n*(n-1)/2
+        bj = j;                        // (n-1) + (n-2) + ... + 1 = n*(n-1)/2
+      }
+    }
+    items.swap(i, bj);            // (n-1)
+  }
+  //                               T(n) = 3*(n-1) + 3*n*(n-1)/2
+  //                                    = 3*n - 3 + (3/2)*n^2 - (3/2)*n
+  //                                    = (3/2)*n^2 + (3/2)*n - 3
+  //                                    = Theta(n^2)
+}
+
+#[test]
+fn test_selection_sort() {
+  let mut v: Vec<i32> = Vec::from([7, 4, 0, 8, 2, 5, 9, 1, 3, 6]);
+  selection_sort(&mut v[..]);
+  assert!(v[0] == 0);
+  assert!(v[1] == 1);
+  assert!(v[2] == 2);
+  assert!(v[3] == 3);
+  assert!(v[4] == 4);
+  assert!(v[5] == 5);
+  assert!(v[6] == 6);
+  assert!(v[7] == 7);
+  assert!(v[8] == 8);
+  assert!(v[9] == 9);
+}
